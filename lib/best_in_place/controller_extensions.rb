bd7a7936e6099ceb166217288f639fadb928894d
Saxon Unrue <saxon@unrue.com>
2013-06-05 15:03:45 -0400

    Letting users overwrite the parameter name, just like :object_name

--- lib/best_in_place/controller_extensions.rb
+++ lib/best_in_place/controller_extensions.rb
@@ -1,18 +1,18 @@
 module BestInPlace
   module ControllerExtensions
-    def respond_with_bip(obj)
-      obj.changed? ? respond_bip_error(obj) : respond_bip_ok(obj)
+    def respond_with_bip(obj, options = {})
+      obj.changed? ? respond_bip_error(obj) : respond_bip_ok(obj, options)
     end
 
   private
-    def respond_bip_ok(obj)
+    def respond_bip_ok(obj, options)
       if obj.respond_to?(:id)
         klass = "#{obj.class}_#{obj.id}"
       else
         klass = obj.class.to_s
       end
       param_key = BestInPlace::Utils.object_to_key(obj)
-      updating_attr = params[param_key].keys.first
+      updating_attr = options[:object_name] || params[param_key].keys.first
 
       if renderer = BestInPlace::DisplayMethods.lookup(klass, updating_attr)
         render :json => renderer.render_json(obj)
